d54bf1ba6ebdc4378f10489764f4d972644a09c6
jordi.bru <jordi.bru@mango.com>
2024-05-26 20:34:48 +0200

    fix estif

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 # More on: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#attribute-reference
 output "certificate_arn" {
-  value = aws_acm_certificate_validation.validations[0].certificate_arn[0]
+  value = aws_acm_certificate_validation.validations[*].certificate_arn
 }
 
 output "domain_validation_options" {
