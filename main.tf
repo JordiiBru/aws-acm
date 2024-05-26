fe55a518e27d6c27b99366e225a7a7bf27af803a
jordi.bru <jordi.bru@mango.com>
2024-05-26 19:39:27 +0200

    fix estif

--- main.tf
+++ main.tf
@@ -15,11 +15,29 @@ resource "aws_acm_certificate" "domain_certificate" {
   }
 }
 
+# record validation
+resource "aws_route53_record" "cert_validations" {
+  for_each =  {
+    for dvo in aws_acm_certificate.domain_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.hosted_zone.zone_id
+}
+
 resource "aws_acm_certificate_validation" "validations" {
   count                   = var.validate_cert ? 1 : 0
   certificate_arn         = aws_acm_certificate.domain_certificate.arn
-  #validation_record_fqdns = [for record in aws_route53_record.cert_validations : record.fqdn]
-  validation_record_fqdns = [for record in var.cert_record : record.fqdn]
+  validation_record_fqdns = [for record in aws_route53_record.cert_validations : record.fqdn]
+  #validation_record_fqdns = [for record in var.cert_record : record.fqdn]
 
   provider        = aws.us-east-1-acm
 }
