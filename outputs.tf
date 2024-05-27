c157a37e0b62a3249fe51d47ad7f2b47697ab97b
jordi.bru <jordi.bru@mango.com>
2024-05-27 20:22:08 +0200

    add: outputs improved

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
 # More on: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket#attribute-reference
 output "certificate_arn" {
-  value = aws_acm_certificate_validation.validations.certificate_arn
+  description = "The ARN of the validated ACM certificate."
+  value       = aws_acm_certificate_validation.validations.certificate_arn
 }
 
 output "domain_validation_options" {
-  value = aws_acm_certificate.domain_certificate[*].domain_validation_options
+  description = "The domain validation options for the ACM certificate."
+  value       = aws_acm_certificate.domain_certificate[*].domain_validation_options
 }
